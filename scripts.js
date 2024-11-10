58f8728aaa5aebdf080be6e4a7940a4fea6bace7
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-11-10 23:26:03 +0200

    Aded Exercise 4,5,6,and 7

--- scripts.js
+++ scripts.js
@@ -36,3 +36,31 @@ console.log(Provinces_upper);
 const Name_length = names.map(name => name.length);
 console.log(Name_length);
 
+//Exercise 4
+//**Sorting**: Use `sort` to alphabetically sort the provinces.
+
+const Provinces_sorted = provinces.sort();
+console.log(Provinces_sorted);
+
+//Exercise 5
+//**Filtering Cape**: Use `filter` to remove provinces containing "Cape". Log the count of remaining provinces.
+
+const Not_Cape_provinces = provinces.filter(province => !province.includes('Cape'));
+console.log(Not_Cape_provinces);
+console.log(Not_Cape_provinces.length);
+
+//Exercise 6
+//**Finding 'S'**: Create a boolean array using `map` and `some` to determine if a name contains the letter 'S'.
+
+const Contains_S = names.map(name => name.includes('S'));
+console.log(Contains_S);
+console.log(Contains_S.some(contains => contains === true));
+
+//Exercise 7
+// **Creating Object Mapping**: Use `reduce` to transform the names array into an object mapping names to their respective provinces.
+
+const Name_to_province = names.reduce((acc, name, index) => {
+  acc[name] = provinces[index];
+  return acc;
+}, {});
+console.log(Name_to_province);
